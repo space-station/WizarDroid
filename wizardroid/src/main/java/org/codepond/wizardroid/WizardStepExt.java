9221c9339fad852e48adf66cbcb3ae08e5a685e4
Cesar Mauri <cesar@crea-si.com>
2019-04-16 12:21:51 +0200

    Force wizard steps to wait for a while
    When users tried to go across the wizard steps too fast
    this caused misbehavior.

--- wizardroid/src/main/java/org/codepond/wizardroid/WizardStepExt.java
+++ wizardroid/src/main/java/org/codepond/wizardroid/WizardStepExt.java
@@ -1,19 +1,28 @@
 package org.codepond.wizardroid;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Simplify implementation of wizard steps
  */
 public abstract class WizardStepExt extends WizardStep {
     private static final String PARAM_ENTERED = "wizard_step_entered";
+
+    private static final int BUTTON_DELAY_MS = 500;
+
     private boolean mEntered = false;   // true when the user is in this wizard
 
     private boolean mInitialized = false;
+
+    @NonNull
+    private final Handler mHandler = new Handler();
 
     public WizardStepExt() { }
 
@@ -34,6 +43,7 @@ public abstract class WizardStepExt extends WizardStep {
     public void onEnter() {
         mEntered = true;
         if (!mInitialized) {
+            hideButtons();
             initialize();
             mInitialized = true;
         }
@@ -43,6 +53,7 @@ public abstract class WizardStepExt extends WizardStep {
     public void onResume() {
         super.onResume();
         if (mEntered && !mInitialized) {
+            hideButtons();
             initialize();
             mInitialized = true;
         }
@@ -58,19 +69,50 @@ public abstract class WizardStepExt extends WizardStep {
     public void onPause() {
         super.onPause();
         if (mInitialized) {
-            deinitialize();
             mInitialized = false;
+            showButtons.run();
+            deinitialize();
         }
     }
 
     @Override
     public void onExitStep() {
         if (mInitialized) {
-            deinitialize();
             mInitialized = false;
+            showButtons.run();
+            deinitialize();
         }
         mEntered = false;
     }
+
+    private void hideButtons() {
+        Activity a = getActivity();
+        if (null != a) {
+            Button button = a.findViewById(R.id.wizard_next_button);
+            button.setVisibility(View.INVISIBLE);
+
+            button = a.findViewById(R.id.wizard_previous_button);
+            button.setVisibility(View.INVISIBLE);
+
+            mHandler.postDelayed(showButtons, BUTTON_DELAY_MS);
+        }
+    }
+
+    private Runnable showButtons = new Runnable() {
+        @Override
+        public void run() {
+            mHandler.removeCallbacks(showButtons);
+
+            Activity a = getActivity();
+            if (null != a) {
+                Button button = a.findViewById(R.id.wizard_next_button);
+                button.setVisibility(View.VISIBLE);
+
+                button = a.findViewById(R.id.wizard_previous_button);
+                button.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 
     protected abstract View doOnCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                                            Bundle savedInstanceState);
